c3458070add527d57774479e80479806c26e85c9
vaguettibfd <vaguetti.bfd@gmail.com>
2026-09-10 17:19:56 -0300

    Atualização do favicon.ico

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/ebook1-logo.svg',
       colorMode: {
         respectPrefersColorScheme: true,
       },
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -25,7 +25,7 @@ const FeatureList = [
   },
   {
     title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Svg: require('@site/static/ebook1-logo.svg.svg').default,
     description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
